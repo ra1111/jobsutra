bce6b09d7651821ee990d89e4988c115d87c2bb8
ra1111 <rahula.raj2013@gmail.com>
2019-06-26 11:03:27 +0530

    Updates

---
+++ precache-manifest.462dedb4ad06a79aac7745003d9e4aa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed5dda75b0328d5fc979e43687f829c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4235b9fcfb0208f2b81c",
+    "url": "/static/css/main.75c9faf8.chunk.css"
+  },
+  {
+    "revision": "cfe0fc8ae172d31a5c1b",
+    "url": "/static/js/2.f258d856.chunk.js"
+  },
+  {
+    "revision": "4235b9fcfb0208f2b81c",
+    "url": "/static/js/main.4a9cbcc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "192b8d3863d7309b369d55baf09458a0",
+    "url": "/static/media/bg_1.192b8d38.jpg"
+  },
+  {
+    "revision": "aa0d9db23496d72541678510cab514a4",
+    "url": "/static/media/undraw_work_time_lhoj.aa0d9db2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jobsutra/precache-manifest.dd69b48745bce2f22bd8a4d21d77667e.js"
+  "/precache-manifest.462dedb4ad06a79aac7745003d9e4aa6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jobsutra/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
