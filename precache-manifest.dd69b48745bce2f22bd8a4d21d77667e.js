59a4a7d3e685285ede58e85d70455c9cb87281f8
ra1111 <rahula.raj2013@gmail.com>
2019-06-26 10:51:05 +0530

    Updates

---
+++ precache-manifest.dd69b48745bce2f22bd8a4d21d77667e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccafb7215f01bab019cd9a71ea98ed15",
+    "url": "/jobsutra/index.html"
+  },
+  {
+    "revision": "4235b9fcfb0208f2b81c",
+    "url": "/jobsutra/static/css/main.75c9faf8.chunk.css"
+  },
+  {
+    "revision": "cfe0fc8ae172d31a5c1b",
+    "url": "/jobsutra/static/js/2.f258d856.chunk.js"
+  },
+  {
+    "revision": "4235b9fcfb0208f2b81c",
+    "url": "/jobsutra/static/js/main.4a9cbcc8.chunk.js"
+  },
+  {
+    "revision": "177bc918a47d50a9f797",
+    "url": "/jobsutra/static/js/runtime~main.efdcfa16.js"
+  },
+  {
+    "revision": "192b8d3863d7309b369d55baf09458a0",
+    "url": "/jobsutra/static/media/bg_1.192b8d38.jpg"
+  },
+  {
+    "revision": "aa0d9db23496d72541678510cab514a4",
+    "url": "/jobsutra/static/media/undraw_work_time_lhoj.aa0d9db2.svg"
+  }
+]);
